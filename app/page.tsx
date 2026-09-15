471cbed2fcc6634c6ccf4f5875d2724657568a87
김정현 <shshjang14@gmail.com>
2026-09-15 11:20:54 +0900

    이름 추가

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,13 @@ export default function Home() {
           Server Component
         </span>
         <h1 className="text-3xl font-semibold tracking-tight text-black dark:text-zinc-50">
-          웹서버보안프로그래밍
+          웹서버보안프로그래밍 - 김정현
         </h1>
         <p className="max-w-md text-base leading-7 text-zinc-600 dark:text-zinc-400">
           create-next-app으로 만든 Next.js 16 App Router 템플릿입니다. 이
           페이지는 서버에서 렌더링되고, 아래 카운터는 브라우저에서 동작하는
-          별도의 Client Component입니다.
+          별도의 Client Component입니다. 열심히 수업을 들으면서 따라해보고
+          있습니당.
         </p>
         <Counter />
 
